ac7c848fb08845065c35cbc06571bc2becad06e4
Michael A. Agarenzo <michaelagarenzo@mail.adelphi.edu>
2018-10-02 14:44:37 -0400

    Add files via upload

---
+++ drop_and_dump_table.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# Variables
+HOST={empty}
+DATABASE={empty}
+HOST_LOG_IN='-u {empty} -p{empty}'
+LOCAL_LOG_IN='-u {empty} -p{empty}'
+
+#Functions
+main () {
+	#Drop Table
+	echo -e "\nDropping $@\n"
+	mysql ${LOCAL_LOG_IN} -D ${DATABASE} -e "DROP TABLE $@"
+
+	#Dump Table Data
+	echo -e "\nDumping $@"
+	dump $@
+}
+
+dump () {
+	mysqldump -h ${HOST} ${HOST_LOG_IN} ${DATABASE} $@ | mysql ${LOCAL_LOG_IN} ${DATABASE}
+}
+
+# Start of script
+echo "Starting Script"
+
+for i in "$@"
+do
+	main $i
+done
+
+echo -e "\nScript Completed"
+#End of script
