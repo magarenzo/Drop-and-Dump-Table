a9d265aa31c3369389d0dfa82c7cc74f05e7765b
Michael A. Agarenzo <michaelagarenzo@mail.adelphi.edu>
2020-07-22 20:27:24 -0400

    Add help options

--- src/drop-dump-table.sh
+++ src/drop-dump-table.sh
@@ -1,19 +1,39 @@
 #!/bin/bash
+###
+### Drop table and dump data from external database to local for any amount of user-specified tables
+###
+### Usage:
+### - https://github.com/magarenzo/Drop-and-Dump-Table
+###
+### Options:
+### -h / -help / --help = Show this message
+###
 
-#Variables
+# Start of help
+help() {
+    sed -rn 's/^### ?//;T;p' "$0"
+}
+
+if [[ $# == 0 ]] || [[ "$1" == "-h" || "$1" == "-help" || "$1" == "--help" ]]; then
+    help
+    exit 1
+fi
+# End of help
+
+# Variables
 HOST={empty}
 HOST_DATABASE={empty}
 HOST_LOG_IN='-u {empty} -p{empty}'
 LOCAL_DATABASE={empty}
 LOCAL_LOG_IN='-u {empty} -p{empty}'
 
-#Functions
+# Functions
 main () {
-	#Drop Table
+	# Drop Table
 	echo -e "\nDropping $@\n"
 	mysql ${LOCAL_LOG_IN} -D ${LOCAL_DATABASE} -e "DROP TABLE $@"
 
-	#Dump Table Data
+	# Dump Table Data
 	echo -e "\nDumping $@"
 	dump $@
 }
@@ -21,7 +41,7 @@ dump () {
 	mysqldump -h ${HOST} ${HOST_LOG_IN} ${HOST_DATABASE} $@ | mysql ${LOCAL_LOG_IN} ${LOCAL_DATABASE}
 }
 
-#Start of script
+# Start of script
 echo "Starting Script"
 
 for i in "$@"
@@ -30,4 +50,4 @@ do
 done
 
 echo -e "\nScript Completed"
-#End of script
+# End of script
